7e0e4e918434cb8267cea9bbf0654e0f656f708d
IsaacAlves7 <isaac.alves0720@gmail.com>
2025-08-09 18:13:09 -0300

    Getting started Kivy!

---
+++ kivy/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App 
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class TheKivyApp(App):
+    pass
+
+# Running the application
+TheKivyApp().run()
